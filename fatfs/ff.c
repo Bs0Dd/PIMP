feb58224529977742a3e9b811a49ddea7274a67b
Bs0Dd <vladislav.z.kz@gmail.com>
2026-01-31 05:39:17 +0100

    Dumb EOF check when reading

--- fatfs/ff.c
+++ fatfs/ff.c
@@ -3836,9 +3836,11 @@ FRESULT f_read_byte (
 	//if (res != FR_OK || (res = (FRESULT)fp->err) != FR_OK) LEAVE_FF(fs, res);
 	//if (!(fp->flag & FA_READ)) LEAVE_FF(fs, FR_DENIED);
 
-	//if (fp->obj.objsize - fp->fptr<=0) {
-	//	LEAVE_FF(fs, FR_DISK_ERR);
-	//}
+	if (fp->obj.objsize - fp->fptr<=0) {
+		*buff = 0xFF; // end of "cartridge", just return 0xFF :-)
+		fp->fptr++;
+		LEAVE_FF(fs, FR_OK);
+	}
 
 	if ((fp->fptr % SS(fs)) == 0) {
 		uint32_t csect = (fp->fptr / SS(fs) & (fs->csize - 1));
